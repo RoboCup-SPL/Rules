982068f9acabd7ba7ea0da71f4c236e1412640fb
Katie Genter <KatieGenter@605e0214-5419-0410-8518-eac4bf5b8dd4>
2013-03-24 20:48:20 +0000

    modifying challenge document based on AustinVilla comments

---
+++ Challenges/pickup.h
@@ -0,0 +1,38 @@
+#ifndef PICKUP_H
+#define PICKUP_H
+
+static const int PICKUP_UDP_PORT = 9000;
+
+struct PickUpSoccerBroadcastInfo {
+    char header[4];    // "PkUp"
+    int playerNum;     // 1-5
+    int team;          // 0 is red 1 is blue
+
+    // position of robot
+    // coordinates in centimetres
+    // 0,0 is in centre of field
+    // +ve x-axis points towards the goal we are atempting to score on
+    // +ve y-axis is 90 degrees counterclockwise from the +ve x-axis
+    // angles in radians, 0 along the +x axis, increasing counterclockwise
+
+    float pos[3];      // x,y,theta
+
+    // main diagonal of covariance matrix of position of robot
+    // i.e. variance in x, y and theta without any correlation information
+
+    float posVar[3];
+
+    // Ball information
+    int ballAge;        // frames since this robot last saw the ball. -1 if we haven't seen it
+
+    // position of ball (same coordinate system as above, without heading)
+
+    float ball[2];
+
+    // main diagonal of covariance of position of ball
+
+    float ballvar[2];
+
+};
+
+#endif // PICKUP_H
